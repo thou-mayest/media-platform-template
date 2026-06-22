b3aa0805eb77f609ddba371b737af1a6612d332a
Amine Brahmi <mbrahmi@reiz.tech>
2026-06-22 19:26:03 +0100

    shared and user entity

--- Users.Presentation/Class1.cs
+++ Users.Presentation/Class1.cs
@@ -1,7 +1,13 @@
 ﻿namespace Users.Presentation
 {
-    public class Class1
+    public static class UserUserModule
     {
-
+        //public static void ConfigureUserModule(this IServiceCollection services, IConfiguration configuration)
+        //{
+        //    // Add services related to the User module here
+        //    // For example, you can register your UserService, UserRepository, etc.
+        //    // services.AddScoped<IUserService, UserService>();
+        //    // services.AddScoped<IUserRepository, UserRepository>();
+        //}
     }
 }
