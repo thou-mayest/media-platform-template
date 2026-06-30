eaaead4532c4c74773fb6a2fb0259b5d207869d8
Ala-dev007 <alaeddinemag@gmail.com>
2026-06-30 15:06:29 +0200

    feat: add Mapperly source generator for User → UserDto mapping

---
+++ src/Users.Application/Users/UserMapper.cs
@@ -0,0 +1,11 @@
+﻿using Riok.Mapperly.Abstractions;
+using Users.Domain;
+
+namespace Users.Application.Users;
+
+[Mapper]
+internal static partial class UserMapper
+{
+
+    internal static partial UserDto ToDto(this User user);
+}
