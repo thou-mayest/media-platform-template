2229fe108c4b5cfa093108f2016d3045768b6c00
ben smida chaker <chaker.smida1@gmail.com>
2026-08-26 22:44:44 +0200

    outbox pattern

--- src/Users.Infrastracture/Persistence/UsersDbContext.cs
+++ src/Users.Infrastracture/Persistence/UsersDbContext.cs
@@ -1,13 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using Users.Domain;
+using SharedKernel.Messaging;
 namespace Users.Infrastracture.Persistence;
 
-internal class UsersDbContext : DbContext
+public class UsersDbContext : DbContext
 {
     public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options)
     {
     }
     public DbSet<User> Users { get; set; }
+    public DbSet<OutboxMessage> OutboxMessages { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema("Users");
@@ -39,6 +41,25 @@ internal class UsersDbContext : DbContext
                 .IsRequired()
                 .HasConversion<string>()
                 .HasMaxLength(100);
+        });
+        modelBuilder.Entity<OutboxMessage>(builder =>
+        {
+            builder.ToTable("OutboxMessages");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Type)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            builder.Property(x => x.Content)
+                .IsRequired();
+
+            builder.Property(x => x.OccurredOnUtc)
+                .IsRequired();
+
+            builder.Property(x => x.Error)
+                .HasMaxLength(2000);
         });
         base.OnModelCreating(modelBuilder);
     }
