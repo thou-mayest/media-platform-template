f783f9abb55f12e099050102cea59d0f218fd765
thou-mayest <85231096+thou-mayest@users.noreply.github.com>
2026-09-16 10:24:04 +0100

    upload tests

--- src/Storage.Application/AssemblyInfo.cs
+++ src/Storage.Application/AssemblyInfo.cs
@@ -3,3 +3,7 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Storage.Infrastracture")]
 [assembly: InternalsVisibleTo("Storage.Presentation")]
 [assembly: InternalsVisibleTo("CleanModular.ArchTests")]
+[assembly: InternalsVisibleTo("Storage.Application.UnitTests")]
+[assembly: InternalsVisibleTo("Storage.Presentation.UnitTests")]
+[assembly: InternalsVisibleTo("Storage.IntegrationTests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
--- src/Storage.Presentation/AssemblyInfo.cs
+++ src/Storage.Presentation/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Host.WebApi")]
 [assembly: InternalsVisibleTo("CleanModular.ArchTests")]
+[assembly: InternalsVisibleTo("Storage.Presentation.UnitTests")]
